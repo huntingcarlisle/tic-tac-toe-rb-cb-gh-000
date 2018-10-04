a59c7eed7f34f7215d032b2224005fbcf5cf7f15
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-04 18:55:58 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -108,7 +108,7 @@ def play(board)
   end
     
   if won?(board)
-    puts "Congratulations #{current_player(board)}!"
+    puts "Congratulations #{winner(board)}!"
   else
     puts "Cat's Game!"
   end
